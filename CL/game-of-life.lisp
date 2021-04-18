76276687b41df91c55b8c96c6668010b7fefbd07
foggynight <foggynight709@gmail.com>
2021-04-18 09:38:30 -0230

    Going to bed -- Will fix this later

--- CL/game-of-life.lisp
+++ CL/game-of-life.lisp
@@ -5,20 +5,32 @@
 ;; Copyright (C) 2021 Robert Coffey
 ;; Released under the MIT license
 
-(defparameter *sw* 40)  ; Screen width
-(defparameter *sh* 24)  ; Screen height
+(defparameter *grid-width* 40)   ; Cell grid width in characters
+(defparameter *grid-height* 24)  ; Cell grid height in characters
 
-(defparameter *cells*
-  (make-list *sh*
-             :initial-element (make-list *sw*
-                                         :initial-element 0)))
-(defparameter *snapshot* '())
+(defparameter *sleep-time* 0.1)  ; Number of seconds to sleep between updates
 
 (defun draw-cells (cells)
-  (loop for i in cells do
-    (loop for j in i do
-      (princ j))
-    (fresh-line)))
+  ;; THIS IS THE PROBLEM
+  (loop for i from 0 to *grid-height* do
+    (loop for j from 0 to *grid-width* do
+      (print (aref cells i j)))
+      ;(fresh-line)
+        ))
+
+(defun copy-cells (cells))
+
+(defun step-cells (cells)
+  (let ((snapshot (copy-cells cells)))
+    (loop for row in snapshot do
+      (loop for cell in row do
+        (print cell)))))
 
 (defun main ()
-  (draw-cells *cells*))
+  (let ((cells (make-array (list *grid-height* *grid-width*)
+                           :initial-element 0)))
+    (loop while T do
+      (draw-cells cells)
+      ;(setq cells (step-cells cells))
+      ;(terpri)
+      (sleep *sleep-time*))))
