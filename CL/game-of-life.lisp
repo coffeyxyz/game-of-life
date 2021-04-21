3602a6f38a838f682e2c334fcfeffdbc65e6a9e7
foggynight <foggynight709@gmail.com>
2021-04-21 03:23:05 -0230

    Add step-cells and comments
    Nearly ready to implement the cell rules.

--- CL/game-of-life.lisp
+++ CL/game-of-life.lisp
@@ -12,10 +12,12 @@
 
 (defparameter *sleep-time* 1.0)  ; Number of seconds to sleep between updates
 
+;; Create a new array of cells
 (defun create-cells ()
   (make-array (list *grid-height* *grid-width*)
               :initial-element 0))
 
+;; Print an array of cells
 (defun draw-cells (cells)
   (terpri)
   (loop for y from 0 below *grid-height* do
@@ -23,6 +25,7 @@
       (prin1 (aref cells y x)))
     (fresh-line)))
 
+;; Create a copy of an array of cells
 (defun copy-cells (cells)
   (let ((copy (create-cells)))
     (loop for y from 0 below *grid-height* do
@@ -31,14 +34,25 @@
               (aref cells y x))))
     copy))
 
-;(defun step-cells (cells)
-  ;(let ((snapshot (copy-cells cells)))
+;; Should a given cell live
+(defun should-live (cells snapshot y x)
+  ;; RULES FOR GOL HERE
+  1))
+
+;; Update an array of cells by one step
+(defun step-cells (cells)
+  (let ((snapshot (copy-cells cells)))
+    (loop for y from 0 below *grid-height* do
+      (loop for x from 0 below *grid-width* do
+        (setf (aref cells y x)
+              (should-live cells snapshot y x))))
+    cells))
 
 (defun main ()
   (let ((cells (create-cells)))
     (loop while T do
       (draw-cells cells)
-      (setq cells (step-cells cells))
+      (setf cells (step-cells cells))
       (sleep *sleep-time*))))
 (main)
 
