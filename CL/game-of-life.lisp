b60535ea69803c88f4e632a359bc4e0c8e71d326
foggynight <foggynight709@gmail.com>
2021-04-21 03:03:08 -0230

    Add create-cells and fix previous change

--- CL/game-of-life.lisp
+++ CL/game-of-life.lisp
@@ -12,25 +12,37 @@
 
 (defparameter *sleep-time* 1.0)  ; Number of seconds to sleep between updates
 
+(defun create-cells ()
+  (make-array (list *grid-height* *grid-width*)
+              :initial-element 0))
+
 (defun draw-cells (cells)
   (terpri)
-  (loop for i from 0 below *grid-width* do
-    (loop for j from 0 below *grid-height* do
-      (prin1 (aref cells i j)))
+  (loop for y from 0 below *grid-height* do
+    (loop for x from 0 below *grid-width* do
+      (prin1 (aref cells y x)))
     (fresh-line)))
 
-(defun copy-cells (cells))
+(defun copy-cells (cells)
+  (let ((copy (create-cells)))
+    (loop for y from 0 below *grid-height* do
+      (loop for x from 0 below *grid-width* do
+        (setf (aref copy y x)
+              (aref cells y x))))
+    copy))
 
-(defun step-cells (cells)
-  (let ((snapshot (copy-cells cells)))
-    (loop for cell in cells do
-      (print cell))))
+;(defun step-cells (cells)
+  ;(let ((snapshot (copy-cells cells)))
 
 (defun main ()
-  (let ((cells (make-array (list *grid-width* *grid-height*)
-                           :initial-element 0)))
+  (let ((cells (create-cells)))
     (loop while T do
       (draw-cells cells)
-      ;(setq cells (step-cells cells))
+      (setq cells (step-cells cells))
       (sleep *sleep-time*))))
 (main)
+
+(defun test ()
+  (let ((c1 (create-cells))
+        (c2 (create-cells)))
+    c1))
