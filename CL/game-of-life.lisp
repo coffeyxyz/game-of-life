58edd4c22692c355ac355121c9cd6681aa157a53
foggynight <foggynight709@gmail.com>
2021-04-21 01:05:32 -0230

    Fix draw-cells array iteration

--- CL/game-of-life.lisp
+++ CL/game-of-life.lisp
@@ -1,3 +1,5 @@
+#!/usr/bin/sbcl --script
+
 ;; --- game-of-life.lisp ---
 ;;
 ;; John Conway's: Game of Life, printed directly to the terminal.
@@ -8,23 +10,21 @@
 (defparameter *grid-width* 40)   ; Cell grid width in characters
 (defparameter *grid-height* 24)  ; Cell grid height in characters
 
-(defparameter *sleep-time* 0.1)  ; Number of seconds to sleep between updates
+(defparameter *sleep-time* 1.0)  ; Number of seconds to sleep between updates
 
 (defun draw-cells (cells)
-  ;; THIS IS THE PROBLEM
-  (loop for i from 0 to *grid-height* do
-    (loop for j from 0 to *grid-width* do
-      (print (aref cells i j)))
-      ;(fresh-line)
-        ))
+  (terpri)
+  (loop for i from 0 below *grid-height* do
+    (loop for j from 0 below *grid-width* do
+      (prin1 (aref cells i j)))
+    (fresh-line)))
 
 (defun copy-cells (cells))
 
 (defun step-cells (cells)
   (let ((snapshot (copy-cells cells)))
-    (loop for row in snapshot do
-      (loop for cell in row do
-        (print cell)))))
+    (loop for cell in cells do
+      (print cell))))
 
 (defun main ()
   (let ((cells (make-array (list *grid-height* *grid-width*)
@@ -32,5 +32,5 @@
     (loop while T do
       (draw-cells cells)
       ;(setq cells (step-cells cells))
-      ;(terpri)
       (sleep *sleep-time*))))
+(main)
